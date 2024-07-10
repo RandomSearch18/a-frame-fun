2b21ae8eb1e8c41641f067a17ecacee8f6c0429b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2024-07-10 19:31:21 +0100

    Allow zooming

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import "aframe"
+
+window.addEventListener("wheel", (event) => {
+  const UPPER_BOUND = 2
+  const LOWER_BOUND = 0.1
+
+  // small increments for smoother zooming
+  const delta = event.deltaY / 120 / 10
+  const cameraElement = document.querySelector("#cam")!
+  const cameraData = cameraElement.getAttribute("camera")
+  let finalZoom = cameraData.zoom - delta
+
+  if (finalZoom < LOWER_BOUND) finalZoom = LOWER_BOUND
+  if (finalZoom > UPPER_BOUND) finalZoom = UPPER_BOUND
+  cameraData.zoom = finalZoom
+  cameraElement.setAttribute("camera", cameraData)
+})
