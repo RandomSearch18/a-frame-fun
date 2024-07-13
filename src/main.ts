d2b32c45aa472d77e0e8be03bd64cb8e7bfbf791
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2024-07-13 12:07:33 +0100

    Allow flight up/down with scroll wheel

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,15 @@ import "aframe"
 import "aframe-extras/controls/index.js"
 
 window.addEventListener("wheel", (event) => {
-  const UPPER_BOUND = 2
-  const LOWER_BOUND = 0.1
+  const UPPER_BOUND = 20
+  const LOWER_BOUND = -0.5
 
   // small increments for smoother zooming
-  const delta = event.deltaY / 120 / 10
-  const cameraElement = document.querySelector("#cam")!
-  const cameraData = cameraElement.getAttribute("camera")
-  let finalZoom = cameraData.zoom - delta
-
+  const delta = event.deltaY / 120 / 2
+  const rigElement = document.querySelector("#rig")!
+  const rigPosition = rigElement.getAttribute("position")
+  let finalZoom = rigPosition.y + delta
   if (finalZoom < LOWER_BOUND) finalZoom = LOWER_BOUND
   if (finalZoom > UPPER_BOUND) finalZoom = UPPER_BOUND
-  cameraData.zoom = finalZoom
-  cameraElement.setAttribute("camera", cameraData)
+  rigPosition.y = finalZoom
 })
