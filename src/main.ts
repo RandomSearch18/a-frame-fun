3cf87490fb7ab981bc75296594ac07e26dff5612
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2024-07-14 00:37:25 +0100

    Make zoom more granular at lower heights and more jumpy at higher heights

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,14 @@ window.addEventListener("wheel", (event) => {
   const LOWER_BOUND = -1.8 + 0.01
 
   // small increments for smoother zooming
-  const delta = event.deltaY / 120 / 2
+  const scrollDelta = event.deltaY / 120 / 2
   const rigElement = document.querySelector("#rig")!
   const rigPosition = rigElement.getAttribute("position")
-  let finalZoom = rigPosition.y + delta
-  if (finalZoom < LOWER_BOUND) finalZoom = LOWER_BOUND
-  if (finalZoom > UPPER_BOUND) finalZoom = UPPER_BOUND
-  rigPosition.y = finalZoom
+  const heightDelta = scrollDelta * Math.max(1, Math.abs(rigPosition.y / 3))
+  let finalHeight = rigPosition.y + heightDelta
+  if (finalHeight < LOWER_BOUND) finalHeight = LOWER_BOUND
+  if (finalHeight > UPPER_BOUND) finalHeight = UPPER_BOUND
+  rigPosition.y = finalHeight
 })
 
 const mapObjectsContainer = document.querySelector("#map-objects")!
